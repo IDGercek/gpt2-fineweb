325e2e2b5cf46077d21e15d976d0ed8e854e9a31
IDGercek <gercekilhan44@gmail.com>
2025-09-08 13:08:42 +0300

    Fix typo in model.py

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ class Attention(nn.Module):
                                           dropout=config.dropout,
                                           batch_first=True)
     def forward(self, x, key_padding_mask=None):
-        out, _ = self.attn(x, x, x, is_casual=True, key_padding_mask=key_padding_mask)
+        out, _ = self.attn(x, x, x, is_causal=True, key_padding_mask=key_padding_mask)
         return out
 
 class Block(nn.Module):
