7fc401331eb62a0fb91317a315940903ab1ff975
IDGercek <gercekilhan44@gmail.com>
2025-09-08 12:06:29 +0300

    Initial commit

---
+++ model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # vocabulary size
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+    hidden_size: int = 4 * 768
+    dropout: float = 0.2
+
+class Attention(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0, f"Embedding dimension ({config.n_embd}) must be divisible by number of heads ({config.n_head})."
+
+        self.attn = nn.MultiheadAttention(embed_dim=config.n_embd,
+                                          num_heads=config.n_head,
+                                          dropout=config.dropout,
+                                          batch_first=True)
+    def forward(self, x, attn_mask=None):
+        out, _ = self.attn(x, x, x, key_padding_mask=attn_mask)
+        return out
+
+class Block(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = Attention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = nn.Sequential(
+            nn.Linear(config.n_embd, config.hidden_size),
+            nn.GELU(),
+            nn.Dropout(config.dropout),
+            nn.Linear(config.hidden_size, config.n_embd),
+            nn.Dropout(config.dropout)
+        )
+
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x, attn_mask=None):
+        # We apply layer normalization before attention to have better gradient flow in the residual stream.
+        x = x + self.dropout(self.attn(self.ln_1(x), attn_mask=attn_mask))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+class GPT(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+
+        self.config = config
+
+        self.tok_embd = nn.Embedding(config.vocab_size, config.n_embd) # Token embeddings
+        self.pos_embd = nn.Embedding(config.vocab_size, config.n_embd) # Positional embeddings
+
+        self.hidden = nn.ModuleList([Block(config) for _ in range(config.n_layer)]) # Transformer blocks
+
+        self.ln_final = nn.LayerNorm(config.n_embd) # Final layer normalization
+
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # classifier
+        self.tok_embd.weight = self.lm_head.weight # Weight sharing
+
+        self.dropout = nn.Dropout(config.dropout) # Dropout
+
+    def forward(self, idx, attn_mask=None):
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        # Token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # (T)
+        # position embeddings --> (T, n_embd)
+        # token embeddings    --> (B, T, n_embd)
+        x = self.dropout(self.transformer.wpe(pos) + self.transformer.wte(idx))
+
+        # Forward the blocks of transformer
+        for block in self.hidden:
+            x = block(x, attn_mask=attn_mask)
+
+        # Final projection
+        x = self.ln_final(x)
+        x = self.lm_head(x)
+
+        return x
---
+++ train.py
@@ -0,0 +1,121 @@
+import torch
+from torch.utils.data import DataLoader
+from datasets import load_dataset, get_dataset_config_info
+from transformers import AutoTokenizer
+from dataclasses import dataclass
+import time
+
+from model import GPT, GPTConfig
+
+## -------- Setup --------
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
+@dataclass
+class TrainingConfig:
+    # Data
+    ds_proportion: float = 0.00001
+    num_workers: int = 2
+    prefetch_factor: int = 4
+
+    # Training
+    batch_size: int = 4
+    learning_rate: float = 1e-3
+    ignore_index: int = -100
+    
+
+config = TrainingConfig()
+
+## -------- Data --------
+
+# Load the dataset and metadata
+dataset = load_dataset("HuggingFaceFW/fineweb", name="sample-10BT", split="train", columns=["text"], streaming=True)
+info = get_dataset_config_info("HuggingFaceFW/fineweb", "sample-10BT")
+
+# Take a spesific proportion
+ds_samples = int(info.splits["train"].num_examples * config.ds_proportion)
+dataset = dataset.take(ds_samples)
+print(f"Dataset initialized with {ds_samples} samples.")
+
+# Tokenization on the fly
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+def tokenize(batch):
+    tokens = tokenizer(
+        batch["text"],
+        truncation=True,
+        padding="max_length",
+        max_length=model_config.block_size,
+    )
+
+    tokens["labels"] = [ [config.ignore_index if tok == tokenizer.pad_token_id else tok for tok in seq] for seq in tokens["input_ids"] ]
+
+    return tokens
+
+dataset = dataset.map(tokenize, batched=True)
+
+dataloader = DataLoader(
+        dataset,
+        batch_size=config.batch_size,
+        num_workers=config.num_workers,
+        prefetch_factor=config.prefetch_factor
+)
+
+## -------- Model --------
+
+model_config = GPTConfig(
+    block_size=1024,
+    vocab_size=50257,
+    n_layer=12,
+    n_head=12,
+    n_embd=768,
+    hidden_size= 4 * 768,
+    dropout=0.2
+)
+model = GPT(model_config).to(device)
+parameter_count = sum([p.numel() for p in model.parameters()])
+print(f"Model initialized with {'{:,}'.format(parameter_count)} parameters")
+
+criterion = torch.nn.CrossEntropyLoss(ignore_index=config.ignore_index)
+optimizer = torch.optim.AdamW(params=model.parameters(), lr=config.learning_rate)
+
+## -------- Training --------
+
+epochs = 1
+max_steps = epochs * ds_samples
+epoch_print_interval = 10
+print(f"Training for {max_steps} steps:")
+
+for step, (input_ids, labels, attn_mask) in enumerate(dataloader):
+    t0 = time.time() # Start time
+
+    # Prepare data
+    # Data comes in shape (max_len, batch_size). We convert it to (batch_size, max_len) with torch.stack
+    input_ids = torch.stack(input_ids, dim=1).to(device)
+    labels = torch.stack(labels, dim=1).to(device)
+    attn_mask = torch.stack(attn_mask, dim=1).to(device)
+
+    t1 = time.time() # Data preparation time
+
+    # Zero gradients
+    optimizer.zero_grad()
+
+    # Forward pass
+    logits = model(input_ids, attn_mask)
+
+    # Backpropagation
+    loss = criterion(logits, labels)
+    loss.backward()
+    optimizer.step()
+
+    # Time calculation
+    torch.cuda.synchronize()
+    t2 = time.time()
+    data_prep = (t1 - t0) * 1000
+    step_time = (t2 - t1) * 1000
+
+    if step % epoch_print_interval == 0:
+        print(f"Step: {step} | Loss: {loss.item():.4f} | Data Prep: {data_prep:.2f}ms | Step Time: {step_time:.2f}ms")
+
